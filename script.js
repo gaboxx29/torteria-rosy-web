41da5e5eff3b4f2d3b36a90fbf52d9a609bff424
gaboxx29 <gabrielhernandezposadas@gmail.com>
2025-07-18 13:51:00 -0600

    Torteria Rosy
    Primera subida completa del sitio web de Tortería Rosy

---
+++ script.js
@@ -0,0 +1,8 @@
+// Inicializa AOS (Animate On Scroll)
+AOS.init({
+    duration: 1000, // Duración de la animación en ms
+    once: true,    // Solo anima una vez al hacer scroll hacia abajo
+});
+
+// Puedes añadir cualquier otro JavaScript interactivo aquí en el futuro
+// Por ejemplo, validaciones de formularios, carruseles de imágenes personalizados, etc.
